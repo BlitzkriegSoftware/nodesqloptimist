ce4a84bd4349f734af29ecb9c0cbb4c0c9713e10
Stuart Williams <spookdejur@hotmail.com>
2024-12-05 16:47:44 -0800

    Edit,1

--- src/index.test.js
+++ src/index.test.js
@@ -93,13 +93,63 @@ describe('regular query', () => {
     const db = BlitzDb.getDb(dbname, password, port);
     expect(db != null).toBe(true);
 
-    const id = await db
+    const result = await db
       .insert({ Name: 'Insert Eggs', Priority: 3 })
       .into('Stuff');
-    console.log(id);
+    console.log('Result', result);
+
+    await db.destroy();
+  });
+
+  test('update', async () => {
+    const dbname = process.env.BLITZSQLNAME ?? '';
+    const password = process.env.BLITZSQLPASS ?? '';
+    const port = parseInt(process.env.BLITZSQLPORT, 10) ?? 0;
+
+    const db = BlitzDb.getDb(dbname, password, port);
+    expect(db != null).toBe(true);
+
+    const result = await db('Stuff')
+      .where('StuffId', '=', 1)
+      .update({ Priority: 1 });
+    console.log('Result', result);
+
+    await db.destroy();
+  });
+
+  test('delete', async () => {
+    const dbname = process.env.BLITZSQLNAME ?? '';
+    const password = process.env.BLITZSQLPASS ?? '';
+    const port = parseInt(process.env.BLITZSQLPORT, 10) ?? 0;
+
+    const db = BlitzDb.getDb(dbname, password, port);
+    expect(db != null).toBe(true);
+    const result = await db('Stuff').where('StuffId', '>', 5).delete();
+    console.log('Result', result);
 
     await db.destroy();
   });
 });
 
-describe('Optimistic Locking', () => {});
+describe('Optimistic Locking', () => {
+  test('Experiment 1', async () => {
+    const dbname = process.env.BLITZSQLNAME ?? '';
+    const password = process.env.BLITZSQLPASS ?? '';
+    const port = parseInt(process.env.BLITZSQLPORT, 10) ?? 0;
+
+    const db = BlitzDb.getDb(dbname, password, port);
+    expect(db != null).toBe(true);
+
+    await db.destroy();
+  });
+  test('Experiment 2', async () => {
+    const dbname = process.env.BLITZSQLNAME ?? '';
+    const password = process.env.BLITZSQLPASS ?? '';
+    const port = parseInt(process.env.BLITZSQLPORT, 10) ?? 0;
+
+    const db = BlitzDb.getDb(dbname, password, port);
+    expect(db != null).toBe(true);
+
+    await db.destroy();
+  });
+});
